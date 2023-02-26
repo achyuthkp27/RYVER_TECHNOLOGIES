743b33c2752448a680d323755cd6c2fe89be2bc3
Achyuth KP <kpachyuthz@gmail.com>
2023-02-26 21:35:14 +0530

    course section

--- js/readJson.js
+++ js/readJson.js
@@ -1,10 +1,7 @@
 // read remote JSON file in javascript
-fetch("https://jsonplaceholder.typicode.com/users")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    for (let i = 0; i < data.length; i++) {
-      console.log(data[i]);
-    }
+fetch("./js/content.json")
+  .then((res) => res.json())
+  .then((data) => {
+    // data.forEach((p) => console.log(p));
+    console.log(data[0].id);
   });
