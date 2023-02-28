d6f8cce280f0537f2df1d3a1554ea4edde2c521e
Achyuth KP <kpachyuthz@gmail.com>
2023-02-28 23:41:45 +0530

    using cdn for all links

--- js/readJson.js
+++
@@ -1,7 +0,0 @@
-// read remote JSON file in javascript
-fetch("./js/content.json")
-  .then((res) => res.json())
-  .then((data) => {
-    // data.forEach((p) => console.log(p));
-    console.log(data[0].id);
-  });
